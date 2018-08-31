892791a4ed3512702d18741f76380fefcfc4fbc1
Felipe <felipe.meriga@gmail.com>
2018-08-30 22:37:54 -0300

    Adding font awesome to the project

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -5,7 +5,6 @@ import '../content/scss/vendor.scss';
 // Imports all fontawesome core and solid icons
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { library } from '@';
 import {
     faUser,
     faSort,
